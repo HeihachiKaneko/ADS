ed124654d227d5cd19f4fbd895b06bc11070b554
HeihachiKaneko <heihachi.kaneko@gmail.com>
2019-11-19 22:18:44 -0300

    materialConstrucao

---
+++ eclipse-workspace/MaterialConstrucao/src/Main.java
@@ -0,0 +1,9 @@
+import interfaceGrafica.InterfaceGrafica;
+
+public class Main {
+	public static void main(String[] args) {
+		InterfaceGrafica interfacegrafica = new InterfaceGrafica();
+		interfacegrafica.menu();
+	}
+	
+}
